71cf1e43617616120d46a22b3c8c551cd58c9b68
Kvecere <kvecere@lions.molloy.edu>
2018-04-12 00:20:47 -0400

    Add files via upload

---
+++ home_files/badge_stars.js
@@ -0,0 +1,1 @@
+(function() {document.write('<iframe src="//greatnonprofits.org/badge/stars'+gnp_num+'_new/'+gnp_url+'" width="135" height="172" frameborder="0" scrolling="no"></iframe>');})()
---
+++ home_files/utilities.js
@@ -0,0 +1,26 @@
+﻿function openlink(url) {openLink(url); }
+function openLink(url) {
+    win = window.open(url, "link", "width=1080,height=600,left=100,scrollbars=yes,resizable=yes,location=yes,menubar=yes,toolbar=yes");
+    win.focus;
+}
+
+function replaceString(value) {
+    return "#" + value;
+}
+
+function goBack() {
+    window.history.back();
+}
+
+function shortString(value) {
+    var text = value;
+    if (text.length > 300)
+        return text.substring(0, 300);
+    else return text;
+}
+
+////hide all non-existent images
+//$("img").error(function () {
+//    //$(this).attr("src", "~/images/photos/nophoto.jpg");
+//    $(this).css({ visibility: "hidden" }); 
+//});
